1c9781c69ef9ea3e21ddda57604b839e5e3534c4
maruf3535 <marufrafsan677@gmail.com>
2026-06-12 22:02:19 +0000

    sent email bug sovled

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,11 +1,4 @@
-import { config } from "dotenv";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-// Walk up from src/ (or dist/) to the project root to find .env
-const __dirname = dirname(fileURLToPath(import.meta.url));
-config({ path: resolve(__dirname, "../../../.env") });
+import "dotenv/config";
 import app from "./app";
 import { logger } from "./lib/logger";
 
